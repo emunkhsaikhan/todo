79a3d8e8586c310ad0ce3463432e3709605a2377
one.munkhsaikhan <one.munkhsaikhan@gmail.com>
2025-03-19 18:18:46 +0800

    added front files

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="text-6xl font-medium text-white">TO-DO LIST APP</h1>
+    </nav>
+  );
+};
